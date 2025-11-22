b4b4d7af68275e71f875ce386edb5b9d83f2c7dc
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2025-11-22 17:48:49 +0100

    adding ft_putnbr

--- ft_printf.h
+++ ft_printf.h
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print.h                                         :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 17:24:43 by alamrani          #+#    #+#             */
-/*   Updated: 2025/11/22 17:26:28 by alamrani         ###   ########.fr       */
+/*   Updated: 2025/11/22 17:44:53 by alamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
-# define FT_PRINT.H
+#ifndef FT_PRINT_H
+# define FT_PRINT_H
 
 #include <unistd.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
