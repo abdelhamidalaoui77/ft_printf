b97e73ae7d07b5796add7b0aeeb9df6885917dfb
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2025-11-22 17:29:15 +0100

    starting ft_printf project

---
+++ ft_print.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 17:24:43 by alamrani          #+#    #+#             */
+/*   Updated: 2025/11/22 17:26:28 by alamrani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef
+# define FT_PRINT.H
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+
+#endif
